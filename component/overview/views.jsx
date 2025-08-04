5a4332683b33f9eeb64a8137d2d2376899465adc
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2025-08-04 18:18:26 +0600

    views , LineChartView,TinyBarChartBox

--- component/overview/views.jsx
+++ component/overview/views.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 import { FaArrowTrendUp } from 'react-icons/fa6'
 
-const StatCard = ({label,amount,percentage})=>{
+const StatCard = ({ label, amount, percentage }) => {
     return <div className='w-[202px] h-28 bg-[#EDEEFC] rounded-2xl px-6 py-6 ' >
-                    <h1 className='text-[14px] '>{label}</h1>
-                    <div className='flex items-center gap-3 '>
-                        <h4 className='py-2 text-2xl '>{amount}</h4>
-                        <div className='flex items-center gap-4  '>
-                            <span className='text-[12px]'>{percentage}%</span>
-                            <FaArrowTrendUp />
-                        </div>
-                    </div>
-                </div>
+        <h1 className='text-[14px] '>{label}</h1>
+        <div className='flex items-center gap-3 '>
+            <h4 className='py-2 text-2xl '>{amount}</h4>
+            <div className='flex items-center gap-4  '>
+                <span className='text-[12px]'>{percentage}%</span>
+                <FaArrowTrendUp />
+            </div>
+        </div>
+    </div>
 }
 
 
@@ -21,34 +21,32 @@ const StatCard = ({label,amount,percentage})=>{
 const Views = () => {
     const statList = [
         {
-            label:"View",
-            amount:5980,
-            percentage:'-88.75'
+            label: "View",
+            amount: 5980,
+            percentage: '-88.75'
         },
         {
-            label:"View",
-            amount:5980,
-            percentage:'-88.75'
+            label: "View",
+            amount: 3671,
+            percentage: '-0.03     '
         },
         {
-            label:"View",
-            amount:5980,
-            percentage:'-88.75'
+            label: "View",
+            amount: 156,
+            percentage: '-15.03 '
         },
         {
-            label:"View",
-            amount:5980,
-            percentage:'-88.75'
+            label: "View",
+            amount: 2318,
+            percentage: '-6.08'
         },
-      
+
     ]
     return (
         <>
-            <div className=' flex  gap-5 flex-wrap'>
+            <div className=' flex  gap-5 flex-wrap py-5'>
 
-                
-          
-           {statList.map((x)=><StatCard label={x.label} amount={x.amount} percentage={x.percentage}/>)}
+                {statList.map((x, i) => <StatCard key={i} label={x.label} amount={x.amount} percentage={x.percentage} />)}
 
             </div>
 
