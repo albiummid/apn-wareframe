77e36c3ae750e2838a7637381b55ae25f3b8a48c
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2025-08-05 16:12:51 +0600

    fix: remove unnecessary whitespace in handleSubmit function

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -18,6 +18,7 @@ export default function LoginScreen() {
         password: string;
     }) => {
         console.log(values);
+        
     };
     return (
         <div className="flex justify-center items-center min-h-screen">
