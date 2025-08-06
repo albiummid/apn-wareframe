78bc9889865082fcfc3b506a5f57cb7ad80abfda
albiummid <albi.ummid@gmail.com>
2025-08-06 16:20:43 +0600

    api integrated with error safety

---
+++ components/providers.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createTheme, MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+import { Bounce, ToastContainer } from "react-toastify";
+export const qc = new QueryClient();
+
+const theme = createTheme({
+    fontFamily: "Open Sans, sans-serif",
+    primaryColor: "cyan",
+});
+export default function Providers(props: PropsWithChildren) {
+    return (
+        <div>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                transition={Bounce}
+            />
+
+            <QueryClientProvider client={qc}>
+                <MantineProvider theme={theme}>
+                    {props.children}
+                </MantineProvider>
+            </QueryClientProvider>
+        </div>
+    );
+}
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,21 @@
 import configs from "@/configs";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
     baseURL: configs.API_BASE_URL,
     timeout: 10 * 1000,
 });
+
+api.interceptors.response.use(
+    (res) => res,
+    (err) => {
+        if (err instanceof AxiosError) {
+            let message =
+                err.response?.data.message ||
+                err.message ||
+                "Something went wrong";
+            throw new Error(message);
+        }
+        throw err;
+    }
+);
